b64fea312a4702d3ba42cfb439c531486afa9c57
abreu-dev <gabrielabreu.dev@gmail.com>
2021-09-18 21:12:22 -0300

    Validation on account username

--- src/api/controllers/account_controller.py
+++ src/api/controllers/account_controller.py
@@ -1,8 +1,7 @@
-from fastapi import APIRouter, Depends
-from starlette.responses import Response, JSONResponse
+from fastapi import APIRouter
+from starlette.responses import JSONResponse
 from starlette.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from uuid import uuid4
-from src.api.auth.auth_bearer import JWTBearer
 from src.api.auth.auth_handler import get_full_token
 from src.application.models.login_user_model import LoginUserModel
 from src.application.models.register_user_model import RegisterUserModel
@@ -14,20 +13,22 @@ from src.infrastructure.repositories.user_repository import UserRepository
 router = APIRouter()
 
 
-@router.get("/", dependencies=[Depends(JWTBearer())])
-async def get():
-    return Response(status_code=HTTP_200_OK)
-
-
 @router.post("/register")
 async def register(register_user_model: RegisterUserModel):
     user_repository = UserRepository()
     validation_result = ValidationResultModel()
 
     if user_repository.query() \
+            .filter_by(username=register_user_model.username) \
+            .first() is not None:
+        validation_result.add_error(f"Username '{register_user_model.username}' is already taken.")
+
+    if user_repository.query() \
             .filter_by(email=register_user_model.email) \
             .first() is not None:
-        validation_result.add_error("Email already in use.")
+        validation_result.add_error(f"Email '{register_user_model.email}' is already taken.")
+
+    if validation_result.errors:
         return JSONResponse(status_code=HTTP_400_BAD_REQUEST,
                             content={"errors": validation_result.errors})
 
--- src/api/controllers/health_check_controller.py
+++ src/api/controllers/health_check_controller.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from starlette.responses import Response
 from starlette.status import HTTP_200_OK
+from src.api.auth.auth_bearer import JWTBearer
 
 router = APIRouter()
 
 
-@router.get("/")
+@router.get("/", dependencies=[Depends(JWTBearer())])
 async def get():
     return Response(status_code=HTTP_200_OK)
